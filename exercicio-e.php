dc10c26723faa665abe8ffeb561b84c3d692c6f0
Diego Nunes <contato@diegonunes.dev>
2020-08-08 16:46:00 -0300

    ✏️ fix exercise-e.php

--- exercicio-e.php
+++ exercicio-e.php
@@ -7,14 +7,16 @@
 
   $string = "Na preparação para a final da Copa do Mundo, a França está em ritmo de recuperação física depois da semifinal contra a Bélgica. Nesta quinta-feira (12/julho/2018), 5 jogadores sequer foram a campo";
 
-  $letras = 0;
-
-  foreach (count_chars($string, 1) as $i => $val) {
-    if ($val != " ") {
-      $letras = $letras + $val;
+  $letras = str_split($string);
+  $qtdLetras = 0;
+  foreach ($letras as $key => $value) {
+    if ($value != " ") {
+      $qtdLetras++;
     }
   }
-
-  echo $letras;
+  echo "Total de letras: " . $qtdLetras;
+  echo "\nTotal de dígitos: " . count(array_filter($letras, 'is_numeric'));
+  $palavras = explode(" ", $string);
+  echo "\nTotal de palavras: " . count($palavras);
 
 ?>
